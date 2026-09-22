9097840480c9231e43d83144da4fd4d72d99cbf0
jasper <gregermn@uc.mail.edu>
2026-09-21 21:11:35 -0400

    Initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    void main() {
+
+        int numToExamine = 14;
+
+        IO.println("If mod 2 is 0 then Even. Else if 1 is Odd.");
+        IO.println(numToExamine % 2);
+
+    }
+}
